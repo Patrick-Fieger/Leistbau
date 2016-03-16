20fa23df540e165fa377925c30a1cab2a394bb57
Patrick Fieger <patrickfieger90@gmail.com>
2016-03-16 12:01:09 +0100

    bug fixing

--- email.php
+++ email.php
@@ -1,26 +1,23 @@
-<?php 
-$name = $_POST['name']; 
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
-$myemail = 'patrickfieger90@gmail.com';
+<?php
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+$myemail = 'leistbau@t-online.de';
+// $myemail = 'patrickfieger90@gmail.com';
 $to = $myemail;
- 
+
 $email_subject = "Kontaktformular: $name";
- 
-$email_body = "Sie haben eine neue Nachricht . ".
- 
+
+$email_body = "Sie haben eine neue Nachricht \n\n".
+
 "Details:\n Name: $name \n ".
- 
+
 "Email: $email_address\n Nachricht: \n $message";
- 
-$headers = "From: $myemail\n";
- 
-$headers .= "Reply-To: $email_address";
- 
-if(!mail($to,$email_subject,$email_body,$headers)){
-	echo "Error!";
-}else{
+
+if(mail($to,$email_subject,$email_body)){
 	echo "OK!";
+}else{
+	echo "Error!";
 }
 
 ?>
